b1155aceec8c0c5dbbdaec8f56baed80e216a162
mulewile <william.chanda@posteo.de>
2023-12-22 23:50:49 +0100

    Added backend handling of form data

--- pages/api/audio/index.js
+++ pages/api/audio/index.js
@@ -1,20 +1,156 @@
 import dbConnect from "../../../database/connect";
 import Audio from "../../../database/models/Audio";
+
 export default async function handler(request, response) {
+  const HTTP_STATUS_OK = 200;
+  const HTTP_STATUS_CREATED = 201;
+  const HTTP_STATUS_BAD_REQUEST = 400;
+  const HTTP_STATUS_METHOD_NOT_ALLOWED = 405;
+  const HTTP_STATUS_INTERNAL_SERVER_ERROR = 500;
+
+  const RESPONSE_AUDIO_CREATED = { status: "audio created" };
+
+  const allowedMethods = ["GET", "POST"];
+
   try {
     await dbConnect();
 
     if (request.method === "GET") {
-      const audios = await Audio.find();
+      const audio = await Audio.find();
+      console.log("Hello Get");
 
-      return response.status(200).json(audios);
-    } else {
+      return response.status(HTTP_STATUS_OK).json(audio);
+    } else if (request.method === "POST") {
+      const audioData = request.body;
+      const {
+        name,
+        type,
+        model,
+        serialnumber,
+        purchasedate,
+        condition,
+        color,
+        availability,
+        departmentlocation,
+      } = audioData;
+      const existingAudio = await Audio.findOne({ serialnumber });
+      if (name === "") {
+        console.error("Validation error: Please provide a valid name");
+
+        return response
+          .status(HTTP_STATUS_BAD_REQUEST)
+          .json({ error: "Please provide a valid name" });
+      }
+
+      if (type === "") {
+        console.error("Validation error: Please provide a valid type");
+
+        return response.status(HTTP_STATUS_BAD_REQUEST).json({
+          error: `"Please provide a valid type"`,
+        });
+      } else {
+        console.info("Type is valid:", type);
+      }
+
+      if (model === "") {
+        console.error("Validation error: Please provide a valid model");
+
+        return response
+          .status(HTTP_STATUS_BAD_REQUEST)
+          .json({ error: "Please provide a valid model" });
+      } else {
+        console.info("Model is valid:", model);
+      }
+
+      if (serialnumber === "") {
+        console.error(
+          "Validation error: Please provide a valid serial number input"
+        );
+
+        return response
+          .status(HTTP_STATUS_BAD_REQUEST)
+          .json({ error: "Please provide a valid serial number input" });
+      }
+      if (existingAudio) {
+        return response.status(HTTP_STATUS_BAD_REQUEST).json({
+          error: `"${serialnumber}" is in the database. Change "${existingAudio}" in the form`,
+        });
+      } else {
+        console.info("Serial Number is valid:", serialnumber);
+      }
+
+      if (purchasedate === "") {
+        console.error(
+          "Validation error: Please provide a valid purchase date input"
+        );
+
+        return response
+          .status(HTTP_STATUS_BAD_REQUEST)
+          .json({ error: "Please provide a valid purchase date input" });
+      } else {
+        console.info("Purchase Date is valid:", purchasedate);
+      }
+
+      if (condition === "") {
+        console.error(
+          "Validation error: Please provide a valid condition input"
+        );
+
+        return response
+          .status(HTTP_STATUS_BAD_REQUEST)
+          .json({ error: "Please provide a valid condition input" });
+      } else {
+        console.info("Condition is valid:", condition);
+      }
+
+      if (color === "") {
+        console.error("Validation error: Please provide a valid color input");
+
+        return response
+          .status(HTTP_STATUS_BAD_REQUEST)
+          .json({ error: "Please provide a valid color input" });
+      } else {
+        console.info("Color is valid:", color);
+      }
+
+      if (availability === "") {
+        console.error(
+          "Validation error: Please provide a valid availability input"
+        );
+
+        return response
+          .status(HTTP_STATUS_BAD_REQUEST)
+          .json({ error: "Please provide a valid availability input" });
+      } else {
+        console.info("Availability is valid:", availability);
+      }
+      if (departmentlocation === "") {
+        console.error(
+          "Validation error: Please provide a valid department or location input"
+        );
+
+        return response.status(HTTP_STATUS_BAD_REQUEST).json({
+          error: "Please provide a valid department or location input",
+        });
+      } else {
+        console.info("Department or Location is valid:", departmentlocation);
+      }
+
+      console.log("Hello", audioData);
+      const audio = await Audio.create(audioData);
+
+      response
+        .status(HTTP_STATUS_CREATED)
+        .json(RESPONSE_AUDIO_CREATED, audio, { success: "Form is valid" });
+    } else if (!allowedMethods.includes(request.method)) {
       return response
-        .status(405)
-        .json({ message: "Method not allowed audio Details" });
+        .status(HTTP_STATUS_METHOD_NOT_ALLOWED)
+        .json({ message: "Method not allowed for audio details" });
     }
   } catch (error) {
     console.error("Error in handler:", error);
-    return response.status(500).json({ message: "Internal Server Error" });
+    return response
+      .status(HTTP_STATUS_INTERNAL_SERVER_ERROR)
+      .json({ message: "Internal Server Error: " + error });
   }
 }
