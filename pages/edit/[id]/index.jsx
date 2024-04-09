d82a6b0ed8869b721e25d83cc8dddf3db52fb25d
mulewile <william.chanda@posteo.de>
2024-04-09 23:00:39 +0200

    implemented routing to edit function

--- pages/edit/[id]/index.jsx
+++ pages/edit/[id]/index.jsx
@@ -1,16 +1,8 @@
-import { useState, useRef } from "react";
 import useSWR from "swr";
 import { useRouter } from "next/router";
-import { v4 as uuidv4 } from "uuid";
-import Button from "@/components/Button";
-import Form from "@/components/Form";
 import { StyledFormContainer } from "@/components/Form";
 import myEditImage from "../../../resources/edit.png";
-import { StyledLink } from "../../video/[id]";
-import StyledErrorMessage from "@/components/ErrorMessage";
-import VideoFormDetails from "@/components/FormComponents/videoForm";
-import AudioFormDetails from "@/components/FormComponents/audioForm";
-import CustomerFormDetails from "@/components/FormComponents/customerForm";
+
 
 export const backgroundStyle = {
   backgroundImage: `url(${myEditImage.src})`,
@@ -25,7 +17,7 @@ export const backgroundStyle = {
   alignItems: "center",
 };
 
-export default function Edit({ ref, formStatus }) {
+export default function Edit() {
 
 
 
@@ -37,10 +29,9 @@ export default function Edit({ ref, formStatus }) {
 
   if (error) {
     return (
-      <ErrorContainer>
-        <ErrorMessage>Error loading data: Administrator will connect to the database soon</ErrorMessage>
-        <ErrorMessage>{error.message}</ErrorMessage>
-      </ErrorContainer>
+      <div>
+        <p>Error loading data: Administrator will connect to the database soon</p>
+      </div>
     );
   }
 
